26c489fdd2326c4ef9093026de5d359bb08a42b9
FrancaTM <francatm@outlook.com>
2020-01-03 22:57:36 -0300

    switch between pages

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -5,6 +5,7 @@ import { Route } from "react-router-dom";
 
 import "./Blog.css";
 import Posts from "./Posts/Posts";
+import NewPost from "./NewPost/NewPost";
 
 class Blog extends Component {
   render() {
@@ -23,6 +24,7 @@ class Blog extends Component {
           </nav>
         </header>
         <Route path="/" exact component={Posts} />
+        <Route path="/new-post" component={NewPost} />
       </div>
     );
   }
